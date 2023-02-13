af739fe8d385b0e1f471092ade6468fce29e9743
alu2daw <mngceg@gmail.com>
2023-02-13 09:04:11 +0100

    Añadiendo sql final xd

--- Examen/examen.sql
+++ Examen/examen.sql
@@ -1,34 +1,79 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 13-02-2023 a las 09:03:17
+-- Versión del servidor: 10.4.25-MariaDB
+-- Versión de PHP: 8.1.10
+
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
+--
+-- Base de datos: `examen`
+--
 CREATE DATABASE IF NOT EXISTS `examen` DEFAULT CHARACTER SET utf8 COLLATE utf8_spanish2_ci;
 USE `examen`;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `examen`
+--
+
+DROP TABLE IF EXISTS `examen`;
+CREATE TABLE IF NOT EXISTS `examen` (
+  `id` text COLLATE utf8_spanish2_ci NOT NULL,
+  `idPregunta` text COLLATE utf8_spanish2_ci NOT NULL,
+  `respondido` text COLLATE utf8_spanish2_ci NOT NULL,
+  `nota` decimal(10,2) NOT NULL,
+  KEY `idPregunta` (`idPregunta`(1024))
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
+
+--
+-- Volcado de datos para la tabla `examen`
+--
+
+INSERT INTO `examen` (`id`, `idPregunta`, `respondido`, `nota`) VALUES
+('ATiIzOQp+yHiK3ppDd+cAV3vw91dH5Jq7zq9OVFm/xEZhDAQ3JAQZKuAa65xzquVkeqqMR/llhBBd33XEQApaw==2023.02.13.09.00.58', '[18, 15, 1]', '[69, 60, 1]', '1.67');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `opciones`
+--
 
 DROP TABLE IF EXISTS `opciones`;
 CREATE TABLE IF NOT EXISTS `opciones` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `idPregunta` int(11) NOT NULL,
-  `enunciado` varchar(255) NOT NULL,
+  `enunciado` varchar(255) COLLATE utf8_spanish2_ci NOT NULL,
   `correcta` tinyint(1) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `pregunta_id` (`idPregunta`)
 ) ENGINE=InnoDB AUTO_INCREMENT=109 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
 
+--
+-- Volcado de datos para la tabla `opciones`
+--
+
 INSERT INTO `opciones` (`id`, `idPregunta`, `enunciado`, `correcta`) VALUES
 (1, 1, 'George Washington', 1),
-(2, 1, 'El primer presidente', 1),
-(3, 1, 'Thomas Jefferson', 0),
-(4, 1, 'James Madison', 0),
+(2, 1, 'Michael Jackson', 0),
+(3, 1, 'Thomas Jefferson', 1),
+(4, 1, 'Rick Astley', 0),
 (5, 2, 'París', 1),
-(6, 2, 'Ciudad del amor', 1),
-(7, 2, 'Marsella', 0),
-(8, 2, 'Lyon', 0),
+(6, 2, 'Almería', 0),
+(7, 2, 'Marsella', 1),
+(8, 2, 'Lyon', 1),
 (9, 3, 'Euro', 1),
 (10, 3, 'Pizza', 0),
 (11, 3, 'Dirham', 0),
@@ -61,10 +106,10 @@ INSERT INTO `opciones` (`id`, `idPregunta`, `enunciado`, `correcta`) VALUES
 (38, 10, 'Está en Oceanía', 1),
 (39, 10, 'Nueva Zelanda', 0),
 (40, 10, 'Filipinas', 0),
-(41, 11, 'Pirámides de Giza', 1),
-(42, 11, 'Esfinge ', 1),
-(43, 11, 'Los cocodrilos del Nilo', 0),
-(44, 11, 'La arena del desierto', 0),
+(41, 11, 'España', 1),
+(42, 11, 'Islandia', 0),
+(43, 11, 'Hungría', 1),
+(44, 11, 'Reino Unido', 0),
 (45, 12, 'Hipócrates', 1),
 (46, 12, 'Alexander Flemming', 0),
 (47, 12, 'Los de Anatomía de Gray', 0),
@@ -113,10 +158,10 @@ INSERT INTO `opciones` (`id`, `idPregunta`, `enunciado`, `correcta`) VALUES
 (90, 23, 'Nilo', 1),
 (91, 23, 'El que está en Egipto', 1),
 (92, 23, 'Mississippi', 0),
-(93, 24, 'Adolfo Suárez', 1),
-(94, 24, 'El rey Emérito', 1),
-(95, 24, 'Felipe VI', 0),
-(96, 24, 'Torcuato Fernández Miranda', 1),
+(93, 24, 'phpMyAdmin', 1),
+(94, 24, 'HeidiSQL', 1),
+(95, 24, 'Oracle XE ', 0),
+(96, 24, 'Mysql Workbench', 1),
 (97, 25, 'Brasil', 1),
 (98, 25, 'México', 0),
 (99, 25, 'Argentina', 0),
@@ -130,25 +175,35 @@ INSERT INTO `opciones` (`id`, `idPregunta`, `enunciado`, `correcta`) VALUES
 (107, 27, 'Shanghai Tower', 0),
 (108, 27, 'El to alto ese que está en Dubai', 1);
 
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `preguntas`
+--
+
 DROP TABLE IF EXISTS `preguntas`;
 CREATE TABLE IF NOT EXISTS `preguntas` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `pregunta` varchar(255) NOT NULL,
+  `pregunta` varchar(255) COLLATE utf8_spanish2_ci NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=91 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
 
+--
+-- Volcado de datos para la tabla `preguntas`
+--
+
 INSERT INTO `preguntas` (`id`, `pregunta`) VALUES
-(1, '¿Quién fue el primer presidente de Estados Unidos?'),
-(2, '¿Cuál es la capital de Francia?'),
+(1, '¿Quiénes han sido presidente de los Estados Unidos?'),
+(2, '¿Cuál de estas ciudades pertenecen \r\na Francia?'),
 (3, '¿Cuál es la moneda oficial de Italia?'),
-(4, '¿Quién descubrió América?'),
+(4, '¿Cómo se llamaban las carabelas de Cristobal Colón?'),
 (5, '¿A qué temperatura hierve el agua?'),
 (6, '¿Qué país es conocido por el palacio de Versalles?'),
 (7, '¿Cuál es la capital de Australia?'),
 (8, '¿Quién escribió la Odisea?'),
 (9, '¿Quién es el autor de \"Don Quijote de la Mancha\"?'),
 (10, '¿Qué país es conocido por la \"Opera de Sydney\"?'),
-(11, '¿Cuáles son los monumentos más famosos de Egipto?'),
+(11, '¿Cuáles de los siguientes países pertenecen a la UE?'),
 (12, '¿Quién es el padre de la medicina?'),
 (13, '¿Qué país es conocido por la Torre Eiffel?'),
 (14, '¿Quién escribió \"Sherlock Holmes\"?'),
@@ -161,12 +216,18 @@ INSERT INTO `preguntas` (`id`, `pregunta`) VALUES
 (21, '¿En qué año se produjo la Revolución Francesa?'),
 (22, '¿Cuál es el país más poblado de África?'),
 (23, '¿Cuál es el río más largo del mundo?'),
-(24, '¿Qué personas fueron relevantes durante la Transición?'),
+(24, '¿Cuáles son GUI de MySQL?'),
 (25, '¿Cuál es el país más grande de América Latina?'),
 (26, '¿Quién pintó la Mona Lisa?'),
 (27, '¿Cuál es el edificio más alto del mundo?');
 
+--
+-- Restricciones para tablas volcadas
+--
 
+--
+-- Filtros para la tabla `opciones`
+--
 ALTER TABLE `opciones`
   ADD CONSTRAINT `opciones_ibfk_1` FOREIGN KEY (`idPregunta`) REFERENCES `preguntas` (`id`);
 COMMIT;
